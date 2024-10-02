0fe122b0f9ff12cf513319e703ef0515efe25af0
Gustavo Andre <guh.andre10@gmail.com>
2024-10-02 15:54:20 -0300

    tela inicial pronta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TelaInicioComponent } from "./tela-inicio/tela-inicio.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TelaInicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
